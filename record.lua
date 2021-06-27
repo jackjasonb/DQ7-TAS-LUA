e0a745603d7c92d9f95902e0f4d21cf72cd9de58
jackjasonb <jackjasonb@gmail.com>
2021-06-27 18:48:08 +0900

    update

--- record.lua
+++ record.lua
@@ -1,7 +1,7 @@
 require "utils"
 
-START_FRAME = 91441
-STOP_FRAME = 92000
+START_FRAME = 150245
+STOP_FRAME = 248050
 
 local function start(frame)
     if frame == START_FRAME then
@@ -23,20 +23,36 @@ local function mainloop()
     local frame = emu.framecount()
 
     while start(frame) do
-        frame = emu.framecount()
         emu.frameadvance()
+        frame = emu.framecount()
     end
 
     local file = io.open("movie.csv", "w")
     console.log("file open")
     while stop(frame) do
-        frame = emu.framecount()
+        -- get values
         local time = getTime()
         local r_number = getRNum()
         local pos_x = getPosX()
         local pos_z = getPosZ()
-        file:write(frame .. "," .. time .. "," .. r_number .. "," .. pos_x .. "," .. pos_z .. "\n")
+        local encount_walk = getEncountWalk()
+        local gold = get_gold()
+        local battle_turn = get_turn_now()
+        local enemy_name_1 = get_enemy_name(1)
+        local enemy_hp_1 = get_enemy_HP(1)
+        local enemy_name_2 =  get_enemy_name(2)
+        local enemy_hp_2 = get_enemy_HP(2)
+        local enemy_name_3 get_enemy_name(3)
+        local enemy_hp_3 = get_enemy_HP(3)
+        local enemy_name_4 get_enemy_name(4)
+        local enemy_hp_4 = get_enemy_HP(4)
+
+        -- write row
+        file:write(frame .. "," .. time .. "," .. r_number .. "," .. pos_x .. "," .. pos_z .. "," .. encount_walk .. "," .. gold .. "," .. battle_turn .. "," .. enemy_name_1 .. "," .. enemy_hp_1 .. "," .. enemy_name_2 .. "," .. enemy_hp_2 .. "," .. enemy_name_3 .. "," .. enemy_hp_3 .. "," .. enemy_name_4 .. "," .. enemy_hp_4 .. "\n")
+
+        -- next step
         emu.frameadvance()
+        frame = emu.framecount()
     end
     file:close()
     console.log("file close")
