7ecbbe2505e481d0ab2536669a8dec36eda359b7
jackjasonb <jackjasonb@gmail.com>
2021-06-27 19:07:09 +0900

    update

--- draw_aviutl.lua
+++ draw_aviutl.lua
@@ -30,10 +30,23 @@ function main()
     local r_num = string.format("%8s", row[3])
     local pos_x = string.format("%5d", tonumber(row[4]))
     local pos_z = string.format("%5d", tonumber(row[5]))
+    local encount_walk = string.format("%6d", tonumber(row[6]))
+    local gold = row[7]
+    local battle_turn = row[8]
+    local enemy_name_1 = row[9]
+    local enemy_hp_1 = row[10]
+    local enemy_name_2 = row[11]
+    local enemy_hp_2 = row[12]
+    local enemy_name_3 = row[13]
+    local enemy_hp_3 = row[14]
+    local enemy_name_4 = row[15]
+    local enemy_hp_4 = row[16]
 
     local text = "Frame: " .. frame .. "\nTime: " .. time .. "\nR_NUM: " .. r_num
     local position = "\nX: " .. pos_x .. "  Z: " .. pos_z
-    obj.mes(text .. position)
+    local enc = "\nEnc_Walk: " .. encount_walk
+    local battle = "\nTurn: " .. battle_turn .. "\n" .. enemy_name_1 .. "  " .. enemy_hp_1
+    obj.mes(text .. position .. enc .. battle)
 end
 
 main()
