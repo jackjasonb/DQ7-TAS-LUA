d011772ba63c0587ca8c3d99eac7351ad4f3a539
jackjasonb <jackjasonb@gmail.com>
2021-10-17 12:37:02 +0900

    update

--- record.lua
+++ record.lua
@@ -52,9 +52,20 @@ local function mainloop()
         local enemy_hp_3 = get_enemy_HP(3)
         local enemy_name_4 = get_enemy_name(4)
         local enemy_hp_4 = get_enemy_HP(4)
+        local player1_exp = get_exp(ADDRESS["ARUS_EXP"])
+        local player2_exp = get_exp(ADDRESS["KIEFER_EXP"])
+        local player3_exp = get_exp(ADDRESS["MARIBEL_EXP"])
+        local player1_lv = get_player_lv(ADDRESS["ARUS_LV"])
+        local player2_lv = get_player_lv(ADDRESS["KIEFER_LV"])
+        local player3_lv = get_player_lv(ADDRESS["MARIBEL_LV"])
 
         -- write row
-        file:write(frame .. "," .. time .. "," .. r_number .. "," .. pos_x .. "," .. pos_z .. "," .. encount_walk .. "," .. gold .. "," .. battle_turn .. "," .. enemy_name_1 .. "," .. enemy_hp_1 .. "," .. enemy_name_2 .. "," .. enemy_hp_2 .. "," .. enemy_name_3 .. "," .. enemy_hp_3 .. "," .. enemy_name_4 .. "," .. enemy_hp_4 .. "\n")
+        file:write(frame .. "," .. time .. "," .. r_number .. "," .. pos_x .. "," .. pos_z .. "," .. 
+                   encount_walk .. "," .. gold .. "," .. battle_turn .. "," .. 
+                   enemy_name_1 .. "," .. enemy_hp_1 .. "," .. enemy_name_2 .. "," .. enemy_hp_2 .. "," .. 
+                   enemy_name_3 .. "," .. enemy_hp_3 .. "," .. enemy_name_4 .. "," .. enemy_hp_4 .. "," .. 
+                   player1_exp .. "," .. player2_exp .. "," .. player3_exp .. "," .. 
+                   player1_lv .. "," .. player2_lv .. "," .. player3_lv .. "\n")
 
         -- next step
         emu.frameadvance()
