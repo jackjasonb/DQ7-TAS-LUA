25e805a938a826f4741895ec56244fceb4891ad9
jackjasonb <jackjasonb@gmail.com>
2023-10-17 03:09:43 +0900

    update

---
+++ draw_aviutl_buttle2.lua
@@ -0,0 +1,37 @@
+CSV_PATH = ".\\script\\movie.csv"
+START_FRAME = 22334
+
+local function split(str, d)
+    local s = str
+    local t = {}
+    local p = "%s*(.-)%s*" .. d .. "%s*"
+    local f = function(v)
+        table.insert(t, v)
+    end
+
+    if s ~= nil then
+        string.gsub(s, p, f)
+        f(string.gsub(s, p, ""))
+    end
+    return t
+end
+
+function main()
+    local csv_row = {}
+    local row_num = 0
+
+    for line in io.lines(CSV_PATH) do
+        csv_row[row_num] = line
+        row_num = row_num + 1
+    end
+
+    local row = split(csv_row[obj.frame], ",")
+    local battle_turn = row[8]
+    local enemy_name_1 = row[9]
+    local enemy_hp_1 = row[10]
+
+    local text = "Turn: " .. battle_turn .. "\n" .. enemy_name_1 .. "  HP: " .. enemy_hp_1
+    obj.mes(text)
+end
+
+main()
